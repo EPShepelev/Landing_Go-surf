e9dfe93d252d92de9a9afa15fb5f68fc2d8051a4
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2019-11-23 07:35:26 +0300

    сделал блок хедер, кроме меню

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,17 @@ $(function(){
   /* подключаем slick слайдер, указываем настройки */
   $('.header__slider').slick({
     infinite: true,
-    fade: true 
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow-left image">', 
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right image">',
+    asNavFor: '.slider-dots'
+  });
+
+  /* внизу хедера подключаем еще слайдер, кторый будет с основным */
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+   slidesToScroll: 4,
+    asNavFor: '.header__slider'
   });
 
   
