8bdc111d04fcc03894cabb95255268efd9cdc675
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2019-12-06 21:10:31 +0300

    сделала немного в блоке surf-slider

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,15 @@ $(function(){
   /* внизу хедера подключаем еще слайдер, кторый будет с основным */
   $('.slider-dots').slick({
     slidesToShow: 4,
-   slidesToScroll: 4,
+    slidesToScroll: 4,
     asNavFor: '.header__slider'
+  });
+
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow-left image">', 
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right image">'
   });
 
   
