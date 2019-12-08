dfb6965cf775d304a329bc96ebaef1647a3b8bed
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2019-12-08 10:53:23 +0300

    начал работу с блоком map

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,11 @@ $(function(){
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow-left image">', 
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right image">',
-    asNavFor: '.slider-dots'
+    asNavFor: '.slider-dotshead'
   });
 
   /* внизу хедера подключаем еще слайдер, кторый будет с основным */
-  $('.slider-dots').slick({
+  $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider'
@@ -19,7 +19,16 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow-left image">', 
-    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right image">'
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right image">',
+    asNavFor: '.slider-map'
+  });
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true
   });
 
   
