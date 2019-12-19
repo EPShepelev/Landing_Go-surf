606201dbdcdde1465c462c59e55d56c6bc185bcb
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2019-12-19 21:17:52 +0300

    закончен блок travel

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,12 @@ $(function(){
     asNavFor: '.surf-slider',
     focusOnSelect: true
   });
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow-left image">', 
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right image">'
+  })
 
   
 });
